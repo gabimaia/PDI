d336a06b8f9b270f6e9a123b65c545244b86fd26
Gabriel Maia <gabriel.maia@pop-rn.rnp.br>
2019-10-15 12:00:14 -0300

    detector de borda

--- Filtros espaciais/mediana.py
+++ Filtros espaciais/mediana.py
@@ -3,9 +3,6 @@ import numpy as np
 
 image = input('digite o nome da imagem de entrada: ')
 img = cv2.imread(image)
-
-suave = np.vstack([ np.hstack([img, cv2.GaussianBlur(img, ( 3, 3), 0)]) ])
-
+suave = np.vstack([ np.hstack([img, cv2.medianBlur(img, ( 3, 3), 0)]) ])
 cv2.imshow("Imagem original e suavisadas pelo filtro Gaussiano", suave)
-
 cv2.waitKey(0)
---
+++ Filtros_dectectores_de_borda/Detector_de_Roberts.py
@@ -0,0 +1,40 @@
+import cv2
+import sys
+import numpy as np
+from scipy import ndimage
+from PIL import Image
+
+roberts_cross_v = np.array( [[ 0, 0, 0 ],
+                             [ 0, 1, 0 ],
+                             [ 0, 0,-1 ]] )
+
+roberts_cross_h = np.array( [[ 0, 0, 0 ],
+                             [ 0, 0, 1 ],
+                             [ 0,-1, 0 ]] )
+
+def load_image( infilename ) :
+    img = Image.open(infilename)
+    img.load()
+    # note signed integer
+    return np.asarray( img, dtype="int32" )
+
+def save_image( data, outfilename ) :
+    img = Image.fromarray( np.asarray( np.clip(data,0,255), dtype="uint8"), "L" )
+    img.save( outfilename )
+
+def roberts_cross( infilename, outfilename ) :
+    image = load_image(infilename)
+
+    vertical = ndimage.convolve( image, roberts_cross_v )
+    horizontal = ndimage.convolve( image, roberts_cross_h )
+
+    output_image = np.sqrt( np.square(horizontal) + np.square(vertical))
+
+    save_image(output_image, outfilename)
+    #borda = np.vstack([np.hstack([image, output_image]) ])
+    #cv2.imshow("Deteccao de borda de Roberts", borda)
+    #cv2.waitKey(0)
+
+infilename = input('digite o nome do arquivo de entrada: ')
+outfilename = input('digite o nome do arquivo de saida: ')
+roberts_cross( infilename, outfilename )
