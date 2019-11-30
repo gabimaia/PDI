7ce303969ee5bab525b3d58e81c0468e4b60a9a2
Gabriel Maia de Macedo <gabimaia>
2019-11-30 19:44:37 -0300

    atualizacao MACE

--- Filtro-MACE/mace.py
+++ Filtro-MACE/mace.py
@@ -1,31 +1,99 @@
 import cv2
+import os
 import numpy as np
-from matplotlib import pyplot as plt 
 import glob
-from scipy.stats import mode , skew , kurtosis
+from math import ceil
+from matplotlib import pyplot as plt
+import matplotlib.image as plt2 
+import glob
 from scipy.fftpack import fft
 
-#projetando filtro mace a partir de um banco de dados
-#projetando matrix X
-#realizar a fft de cada imagem e arranjar nas colunas de uma matriz d x N
-img = cv2.imread('A00.bmp')
-print(img.shape)
+def MACE_filter(path, d): #o parametro "d" serve para redimensionar as imagens de treinamento
+                          #o valor padrao para d foi de 64
 
-#X = np.fft.fft2(img)                                                        #armazenando a fft da imagem
-#X = np.fft.fftshift(X)
-#print(X.shape[0])
-def MACE_filter(img):
+    soma = np.zeros((d**2, 1))
+    X_ = np.zeros((d**2,8))
+    i=0
+    for img in path:
+        n = cv2.imread(img,0)
+        n = cv2.resize(n,(d,d))
+        X = np.fft.fft2(n)                      #fft da imagem                                                        
+        X = np.fft.fftshift(X)
+        X_[:,i] = X.flatten()                      #transforma a fft em um vetor d² e armazana na coluna de uma matrix X de dimensao d²xN
+        soma = soma + np.abs(X.reshape(d**2,1))**2 #soma dos espectros de potencia
+        i = i + 1
 
-    X = np.fft.fft2(img)                                                        #armazenando a fft da imagem
-    X = np.fft.fftshift(X)
-    X = X.reshape(X.shape[0]*X.shape[1], 3)                                     #posiciona valores em um array 1D
-    D = np.diag(X.flatten())                                                      #calcula valor de D
-    D_inversa = np.linalg.inv(D)                                                         #inversa de D
-    Xct = np.conj(np.transpose(X))                                                #conjulgado transposta de X
-    
-    H = np.matmul(D_inversa,np.matmul(X,np.matmul(np.matmul(Xct,D_inversa),X)))                 #calculando H
-    
-    return H.reshape((input.shape[0],input.shape[1]))
+    media = soma/i                                 #Calcula a media dos espectros
+    u = np.ones((i, 1))                            #Vetor coluna de N linhas.
+    D = np.diag(media.flatten())                   #D = diagonal da média do espectro de potencia
+    D_inv = np.linalg.pinv(D)                      #Inversa de D 
 
-#path = glob.glob("/data/*.bmp")
-MACE_filter(img)
+    #Calculando o filtro a partir dos parâmetros D, X e u especificados acima
+    H = np.dot(np.dot(np.dot(D_inv,X_),np.linalg.pinv(np.dot(np.conj(np.transpose(X_)),np.dot(D_inv,X_)))),u) 
+    #H = D1*X*((X+*Di*X)^-1)*u
+    return H
+
+
+
+
+def teste_correlacao(img, filtro, d):
+    #lendo a imagem de teste
+    half = int((d**2)/2)
+    img = cv2.imread(img, 0)
+    img = cv2.resize(img, (d,d))
+    #calculando corrlacao
+    img_fft = np.fft.fft2(img)           #transformada da imagem de teste
+    img_fft = np.fft.fftshift(img_fft)
+    img_fft = img_fft.reshape((d**2,1))  #redimensiona a saida para a dimensao do filtro
+    teste = img_fft * np.conj(filtro)    #multiplica a transformada da imagem pelo conjugado do filtro
+    teste = np.fft.ifftshift(teste)     
+    teste = np.fft.ifft2(teste)          #calcula a inversa   
+    #teste = np.fft.ifft2(teste)
+    teste = np.real(teste)*d*d           #normaliza os valores
+    #x = teste[half]
+    teste[half] = teste[0]
+    teste[0] = 0
+    #print(teste[0])
+    return teste
+
+
+
+imagem_de_teste_1 = plt2.imread('2.jpeg')
+imagem_de_teste_2 = plt2.imread('1.jpeg')
+
+plt.figure(figsize=(5,5))
+plt.axis('off')
+
+plt.subplot(121),plt.imshow(imagem_de_teste_1, cmap = 'gray')
+plt.title('imagem de teste 1'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(imagem_de_teste_2, cmap = 'gray')
+plt.title('imagem de teste 2'), plt.xticks([]), plt.yticks([])
+plt.show()
+
+
+
+path = glob.glob("data_3/*.jpeg") #selecionando a pasta cotendo as imagens para treinamento do filtro, com imagens .jpeg
+i=1
+rows = 1
+columns = ceil(len(path))
+                                    #printando as imagens de treino do filtro
+fig=plt.figure(figsize=(10, 10))
+for img in path:
+    treino = plt2.imread(img)
+    fig.add_subplot(rows, columns, i)
+    plt.axis('off')
+    plt.imshow(treino)
+    i=i+1
+plt.show()
+
+A = MACE_filter(path, 64)  #calcula o filtro MACE passando a pasta com as imagens de treino e dimensão de 64 pixels
+
+B = teste_correlacao('2.jpeg', A, 64) #Teste de correlacao usando a imagem de teste 1
+
+plt.plot((B))                         #plot do grafico de correlacao 2D (opcional 3D)
+plt.show()                            # O teste deu positivo para a imagem de especificada
+
+C = teste_correlacao('1.jpeg', A, 64)                #teste de correlacao para a imagem de teste 2
+
+plt.plot(np.real(C))                                 #plotagem do grafico de correlacao
+plt.show()                                           #O teste deu negativo para a imagem
